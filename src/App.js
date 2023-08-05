539c159616cde5f4d18b85923a28bdfd1967dda0
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-08-05 22:41:19 +0100

    create form element and styled element components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import { useState } from "react";
+
+const initialItems = [
+	{ id: 1, description: "Passports", quantity: 2, packed: false },
+	{ id: 2, description: "Socks", quantity: 12, packed: false },
+	{ id: 3, description: "Power Bank", quantity: 1, packed: true },
+];
+
 export default function App() {
 	return (
 		<div className="app">
@@ -14,15 +22,57 @@ function Logo() {
 }
 
 function Form() {
+	const [description, setDescription] = useState("");
+	const [quantity, setQuantity] = useState(1);
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		console.log(e);
+	}
 	return (
-		<div className="add-form">
+		<form className="add-form" onSubmit={handleSubmit}>
 			<h3>What do you need for your 😍 trip?</h3>
-		</div>
+			<select
+				value={quantity}
+				onChange={(e) => setQuantity(Number(e.target.value))}>
+				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+					<option value={num} key={num}>
+						{num}
+					</option>
+				))}
+			</select>
+			<input
+				type="text"
+				placeholder="Item..."
+				value={description}
+				onChange={(e) => setDescription(e.target.value)}
+			/>
+			<button>Add</button>
+		</form>
 	);
 }
 
 function PackingList() {
-	return <div className="list">LIST</div>;
+	return (
+		<div className="list">
+			<ul>
+				{initialItems.map((item) => (
+					<Item item={item} key={item.id} />
+				))}
+			</ul>
+		</div>
+	);
+}
+
+function Item({ item: { description, quantity, packed } }) {
+	return (
+		<li>
+			<span style={packed ? { textDecoration: "line-through" } : {}}>
+				{quantity} {description}
+			</span>
+			<button>❌</button>
+		</li>
+	);
 }
 
 function Stats() {
