477b9c90c3c78baf4711f4d8d61c11154bb7f284
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-08-06 17:14:12 +0100

    Add deleteItem to PackingList Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-	{ id: 1, description: "Passports", quantity: 2, packed: false },
-	{ id: 2, description: "Socks", quantity: 12, packed: false },
-	{ id: 3, description: "Power Bank", quantity: 1, packed: true },
-];
-
 export default function App() {
 	const [items, setItems] = useState([]);
 
@@ -13,11 +7,15 @@ export default function App() {
 		setItems((items) => [...items, item]);
 	}
 
+	function handleDeleteItem(id) {
+		setItems((items) => items.filter((item) => item.id !== id));
+	}
+
 	return (
 		<div className="app">
 			<Logo />
 			<Form onAddItem={handleAddItem} />
-			<PackingList items={items} />
+			<PackingList items={items} onDeleteItem={handleDeleteItem} />
 			<Stats />
 		</div>
 	);
@@ -36,7 +34,13 @@ function Form({ onAddItem }) {
 
 		if (!description) return;
 
-		const newItem = { quantity, description, packed: false, id: crypto.randomUUID() };
+		const newItem = {
+			quantity,
+			description,
+			packed: false,
+			id: crypto.randomUUID(),
+		};
+		//You can as well use --- id: Date.now()--- in the newItem object
 		onAddItem(newItem);
 
 		setDescription("");
@@ -66,25 +70,25 @@ function Form({ onAddItem }) {
 	);
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
 	return (
 		<div className="list">
 			<ul>
 				{items.map((item) => (
-					<Item item={item} key={item.id} />
+					<Item item={item} key={item.id} onDeleteItem={onDeleteItem}/>
 				))}
 			</ul>
 		</div>
 	);
 }
 
-function Item({ item: { description, quantity, packed } }) {
+function Item({ item: { id, description, quantity, packed }, onDeleteItem }) {
 	return (
 		<li>
 			<span style={packed ? { textDecoration: "line-through" } : {}}>
 				{quantity} {description}
 			</span>
-			<button>❌</button>
+			<button onClick={() => onDeleteItem(id)}>❌</button>
 		</li>
 	);
 }
